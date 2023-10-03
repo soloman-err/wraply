6161c06b8c3bd5b54940246ae904a8db06d20a6c
solo <soloman.err@.com>
2023-10-03 09:08:29 +0600

    product hooks configuration updated with browsers policy

--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -7,14 +7,27 @@ const fetchProducts = async () => {
       `${import.meta.env.VITE_SERVER_URL}/products`
     );
 
+    console.log(response);
+
     if (response.status !== 200) {
       throw new Error('Network response failed!');
     }
 
     return response.data || [];
   } catch (error) {
-    console.error('Error fetching products', error);
-    throw error;
+    // Handle specific HTTP errors
+    if (error.response) {
+      console.error('Error response from server:', error.response.status);
+      throw new Error('Server error occurred. Please try again later.');
+    } else if (error.request) {
+      console.error('No response from server:', error.request);
+      throw new Error('No response from the server. Please try again later.');
+    } else {
+      console.error('Error setting up the request:', error.message);
+      throw new Error(
+        'An error occurred while processing your request. Please try again later.'
+      );
+    }
   }
 };
 
@@ -26,6 +39,9 @@ const useProducts = () => {
     refetch,
   } = useQuery('products', fetchProducts, {
     refetchOnWindowFocus: false,
+    onError: (error) => {
+      console.error('Error fetching products', error);
+    },
   });
 
   return [products, loading, refetch, isError];
